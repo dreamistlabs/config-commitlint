785d22aa28377a391960b646665e63528567d29d
Johnny Hu <jgeehen.hu@gmail.com>
2019-11-18 18:41:01 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+// https://www.conventionalcommits.org/en/v1.0.0/#summary
+// https://commitlint.js.org/#/reference-rules
+module.exports = {
+  parserPreset: 'conventional-changelog-conventionalcommits',
+  rules: {
+    'body-leading-blank': [2, 'always'],
+    'footer-leading-blank': [2, 'always'],
+    'header-case': [2, 'always', 'lower-case'],
+    'header-max-length': [2, 'always', 72],
+    'scope-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],,
+    'scope-empty': [2, 'never'],
+    // 'scope-enum': [2, 'always', []],
+    'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'improvement',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+      ],
+    ],
+  },
+};
