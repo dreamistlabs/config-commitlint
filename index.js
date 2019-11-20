26f4151da2ce5781f7066fc944ae118d7ef2c70f
Johnny Hu <johnny@ciandt.com>
2019-11-20 11:35:40 -0800

    feat(commitlint): add new type-enum breaking and other to commitlint

--- index.js
+++ index.js
@@ -3,35 +3,45 @@
 
 module.exports = {
   rules: {
-    'body-leading-blank': [2, 'always'],
-    'footer-leading-blank': [2, 'always'],
-    'header-case': [2, 'always', 'lower-case'],
-    'header-max-length': [2, 'always', 72],
-    'scope-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
-    'scope-empty': [2, 'never'],
-    // 'scope-enum': [2, 'always', []],
-    'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
-    'subject-empty': [2, 'never'],
-    'subject-full-stop': [2, 'never', '.'],
-    'type-case': [2, 'always', 'lower-case'],
-    'type-empty': [2, 'never'],
-    'type-enum': [
+    "body-leading-blank": [2, "always"],
+    "footer-leading-blank": [2, "always"],
+    "header-case": [2, "always", "lower-case"],
+    "header-max-length": [2, "always", 72],
+    "scope-case": [
       2,
-      'always',
-      [
-        'build',
-        'chore',
-        'ci',
-        'docs',
-        'feat',
-        'fix',
-        'improvement',
-        'perf',
-        'refactor',
-        'revert',
-        'style',
-        'test',
-      ],
+      "never",
+      ["sentence-case", "start-case", "pascal-case", "upper-case"]
     ],
-  },
+    "scope-empty": [2, "never"],
+    // 'scope-enum': [2, 'always', []],
+    "subject-case": [
+      2,
+      "never",
+      ["sentence-case", "start-case", "pascal-case", "upper-case"]
+    ],
+    "subject-empty": [2, "never"],
+    "subject-full-stop": [2, "never", "."],
+    "type-case": [2, "always", "lower-case"],
+    "type-empty": [2, "never"],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "breaking",
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "improvement",
+        "perf",
+        "other",
+        "refactor",
+        "revert",
+        "style",
+        "test"
+      ]
+    ]
+  }
 };
